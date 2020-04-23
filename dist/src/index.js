b546c432038fedb9a54199264c6d62fc7203cc66
pampado <pampado@hotmail.com>
2020-04-23 18:19:49 -0400

    fix

--- dist/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("./app");
-app_1.httpServer.listen(3334);
-app_1.httpsServer.listen(process.env.PORT, function () {
-    console.log("Server running on PORT " + process.env.PORT);
-});
