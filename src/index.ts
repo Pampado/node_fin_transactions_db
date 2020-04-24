72dc472bbc81391be113d2c9036e2ec9fa01cc6a
pampado <pampado@hotmail.com>
2020-04-23 22:10:17 -0400

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
+import * as Sentry from '@sentry/node'
 import httpsServer from './app'
+
+Sentry.init({
+  dsn:
+    'https://d003d408147a45019cd8d0167188f6df@o382649.ingest.sentry.io/5211849',
+})
+
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server running on PORT ${process.env.PORT}`)
+// })
 
 // httpServer.listen(3334, () => {
 //   console.log(`Server running on PORT ${process.env.PORT}`)
@@ -6,7 +16,5 @@ import httpsServer from './app'
 httpsServer.listen(process.env.PORT, () => {
   console.log(`Server running on PORT ${process.env.PORT}`)
 })
-
-// app.listen(process.env.PORT, () => {
-//   console.log(`Server running on PORT ${process.env.PORT}`)
-// })
+// or using CommonJS
+// const Sentry = require('@sentry/node');
