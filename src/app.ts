69c04b56a4e8a8de6f55a01bb6f8480f2d357ade
pampado <pampado@hotmail.com>
2020-04-24 11:49:50 -0400

    no request certificate

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,12 @@ import createConnection from './database'
 const privateKey = fs.readFileSync(`${__dirname}/ssl/selfsigned.key`, 'utf8')
 const certificate = fs.readFileSync(`${__dirname}/ssl/selfsigned.crt`, 'utf8')
 
-const credentials = { key: privateKey, cert: certificate }
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  requestCert: false,
+  rejectUnauthorized: false,
+}
 
 createConnection()
 const app = express()
