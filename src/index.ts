449413ad63514531ba18b808afdff047089e7517
pampado <pampado@hotmail.com>
2020-04-23 23:30:40 -0400

    add sentry

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/node'
 import httpsServer, { httpServer } from './app'
 
 // app.listen(process.env.PORT, () => {
@@ -9,4 +10,11 @@ httpServer.listen(3334, () => {
 })
 httpsServer.listen(process.env.PORT, () => {
   console.log(`Server running on PORT ${process.env.PORT}`)
+})
+// or using CommonJS
+// const Sentry = require('@sentry/node');
+
+Sentry.init({
+  dsn:
+    'https://d003d408147a45019cd8d0167188f6df@o382649.ingest.sentry.io/5211849',
 })
